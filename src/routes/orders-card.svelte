5bb4fc339ec97e7c3a91b5fde29d7b285987753e
Ryan Sobol <hello@ryansobol.com>
2024-10-18 07:04:07 -0700

    Extract orders card from OrdersTabs component into OrdersCard component

---
+++ src/routes/orders-card.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { Badge } from '$lib/components/ui/badge/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Table from '$lib/components/ui/table/index.js';
+
+	import type { Code, Order } from '$lib/types';
+
+	type Props = {
+		orders: Record<Code, Order>;
+		selected: Code;
+	};
+
+	const { orders, selected }: Props = $props();
+
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	});
+</script>
+
+<Card.Root>
+	<Card.Header class="px-7">
+		<Card.Title>Orders</Card.Title>
+		<Card.Description>Recent orders from your store.</Card.Description>
+	</Card.Header>
+
+	<Card.Content>
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head>Customer</Table.Head>
+					<Table.Head class="hidden sm:table-cell">Type</Table.Head>
+					<Table.Head class="hidden sm:table-cell">Status</Table.Head>
+					<Table.Head class="hidden md:table-cell">Date</Table.Head>
+					<Table.Head class="text-right">Amount</Table.Head>
+				</Table.Row>
+			</Table.Header>
+
+			<Table.Body>
+				{#each Object.entries(orders) as [code, order] (order.id)}
+					<Table.Row class={selected === code ? 'bg-accent' : ''}>
+						<Table.Cell>
+							<div class="font-medium">{order.customer.name}</div>
+							<div class="hidden text-sm text-muted-foreground md:inline">
+								{order.customer.email}
+							</div>
+						</Table.Cell>
+
+						<Table.Cell class="hidden sm:table-cell">{order.type}</Table.Cell>
+
+						<Table.Cell class="hidden sm:table-cell">
+							<Badge class="text-xs" variant="secondary">{order.status}</Badge>
+						</Table.Cell>
+
+						<Table.Cell class="hidden md:table-cell">
+							{order.createdAt.toPlainDate().toString()}
+						</Table.Cell>
+
+						<Table.Cell class="text-right">{formatter.format(order.total)}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</Card.Content>
+</Card.Root>
--- src/routes/orders-tabs.svelte
+++ src/routes/orders-tabs.svelte
@@ -2,12 +2,11 @@
 	import File from 'lucide-svelte/icons/file';
 	import ListFilter from 'lucide-svelte/icons/list-filter';
 
-	import { Badge } from '$lib/components/ui/badge/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import * as Card from '$lib/components/ui/card/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import * as Table from '$lib/components/ui/table/index.js';
 	import * as Tabs from '$lib/components/ui/tabs/index.js';
+
+	import OrdersCard from './orders-card.svelte';
 
 	import type { Code, Order } from '$lib/types';
 
@@ -17,11 +16,6 @@
 	};
 
 	const { orders, selected }: Props = $props();
-
-	const formatter = new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency: 'USD'
-	});
 </script>
 
 <Tabs.Root value="week">
@@ -58,50 +52,6 @@
 	</div>
 
 	<Tabs.Content value="week">
-		<Card.Root>
-			<Card.Header class="px-7">
-				<Card.Title>Orders</Card.Title>
-				<Card.Description>Recent orders from your store.</Card.Description>
-			</Card.Header>
-
-			<Card.Content>
-				<Table.Root>
-					<Table.Header>
-						<Table.Row>
-							<Table.Head>Customer</Table.Head>
-							<Table.Head class="hidden sm:table-cell">Type</Table.Head>
-							<Table.Head class="hidden sm:table-cell">Status</Table.Head>
-							<Table.Head class="hidden md:table-cell">Date</Table.Head>
-							<Table.Head class="text-right">Amount</Table.Head>
-						</Table.Row>
-					</Table.Header>
-
-					<Table.Body>
-						{#each Object.entries(orders) as [code, order] (order.id)}
-							<Table.Row class={selected === code ? 'bg-accent' : ''}>
-								<Table.Cell>
-									<div class="font-medium">{order.customer.name}</div>
-									<div class="hidden text-sm text-muted-foreground md:inline">
-										{order.customer.email}
-									</div>
-								</Table.Cell>
-
-								<Table.Cell class="hidden sm:table-cell">{order.type}</Table.Cell>
-
-								<Table.Cell class="hidden sm:table-cell">
-									<Badge class="text-xs" variant="secondary">{order.status}</Badge>
-								</Table.Cell>
-
-								<Table.Cell class="hidden md:table-cell">
-									{order.createdAt.toPlainDate().toString()}
-								</Table.Cell>
-
-								<Table.Cell class="text-right">{formatter.format(order.total)}</Table.Cell>
-							</Table.Row>
-						{/each}
-					</Table.Body>
-				</Table.Root>
-			</Card.Content>
-		</Card.Root>
+		<OrdersCard {orders} {selected} />
 	</Tabs.Content>
 </Tabs.Root>
