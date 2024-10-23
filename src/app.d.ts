f1806091623fd07234313f841cedac0e4977a093
Ryan Sobol <hello@ryansobol.com>
2024-10-22 19:15:51 -0700

    Remove unnecessary PageState type declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,12 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import { Order } from './orders/types';
-
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		interface PageState {
-			selectedOrder?: Order;
-		}
+		// interface PageState {}
 		// interface Platform {}
 	}
 }
