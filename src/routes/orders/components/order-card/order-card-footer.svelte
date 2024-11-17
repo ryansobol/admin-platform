e3e329b4443c787d34a51517befc00abcb595119
Ryan Sobol <hello@ryansobol.com>
2024-11-17 08:45:32 -0800

    Add tests for the OrderCardFooter component

--- src/routes/orders/components/order-card/order-card-footer.svelte
+++ src/routes/orders/components/order-card/order-card-footer.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import { Temporal } from 'temporal-polyfill';
 
-	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
-	import ChevronRight from 'lucide-svelte/icons/chevron-right';
-
-	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as Pagination from '$lib/components/ui/pagination/index.js';
 
 	import type { Order } from '../../types';
 
@@ -32,29 +27,11 @@
 		Temporal.Now.timeZoneId()
 	)}
 
-	<Card.Footer class="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
+	<Card.Footer class="flex flex-row items-center border-t bg-muted/50 px-6 py-3" role="contentinfo">
 		<div class="text-xs text-muted-foreground">
 			Last updated on <time dateTime={updatedAtZoned.toString()}>
 				{updatedAtZoned.toLocaleString(locale, options)}
 			</time>
 		</div>
-
-		<Pagination.Root count={10} class="ml-auto mr-0 w-auto">
-			<Pagination.Content>
-				<Pagination.Item>
-					<Button size="icon" variant="outline" class="h-6 w-6">
-						<ChevronLeft class="h-3.5 w-3.5" />
-						<span class="sr-only">Previous Order</span>
-					</Button>
-				</Pagination.Item>
-
-				<Pagination.Item>
-					<Button size="icon" variant="outline" class="h-6 w-6">
-						<ChevronRight class="h-3.5 w-3.5" />
-						<span class="sr-only">Next Order</span>
-					</Button>
-				</Pagination.Item>
-			</Pagination.Content>
-		</Pagination.Root>
 	</Card.Footer>
 {/if}
