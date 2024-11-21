cd120711c0ad9d737765aa648f3b169764785b26
Ryan Sobol <hello@ryansobol.com>
2024-11-20 16:15:31 -0800

    Hide icons in OrderCardHeader component from screen readers

--- src/routes/orders/components/order-card/order-card-header.svelte
+++ src/routes/orders/components/order-card/order-card-header.svelte
@@ -58,7 +58,7 @@
 
 		<div class="ml-auto flex items-center gap-1">
 			<Button size="sm" variant="outline" class="h-8 gap-1">
-				<Truck class="h-3.5 w-3.5" />
+				<Truck aria-hidden class="h-3.5 w-3.5" />
 				<span class="sr-only sm:not-sr-only xl:sr-only 2xl:not-sr-only 2xl:whitespace-nowrap">
 					Track Order
 				</span>
@@ -67,8 +67,8 @@
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger asChild let:builder>
 					<Button builders={[builder]} size="icon" variant="outline" class="h-8 w-8">
-						<EllipsisVertical class="h-3.5 w-3.5" />
-						<span class="sr-only">More</span>
+						<EllipsisVertical aria-hidden class="h-3.5 w-3.5" />
+						<span class="sr-only">More Actions Dropdown</span>
 					</Button>
 				</DropdownMenu.Trigger>
 
