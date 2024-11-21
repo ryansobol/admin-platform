b4eb5f47bfc21ba5dfe8b1e3c7c66021a811f82b
Ryan Sobol <hello@ryansobol.com>
2024-11-21 11:45:57 -0800

    Add icons to the customer's data terms in the OrderCardContentCustomer component

--- src/routes/orders/components/order-card/order-card-content-customer.svelte
+++ src/routes/orders/components/order-card/order-card-content-customer.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import MailIcon from 'lucide-svelte/icons/mail';
+	import PhoneIcon from 'lucide-svelte/icons/phone';
+	import UserIcon from 'lucide-svelte/icons/user';
+
 	import type { Order } from '../../types';
 
 	type Props = {
@@ -14,21 +18,34 @@
 
 		<dl class="grid gap-3">
 			<div class="flex items-center justify-between">
-				<dt class="text-muted-foreground">Name</dt>
+				<dt class="flex items-center gap-1.5 text-muted-foreground">
+					<UserIcon aria-hidden class="size-4" />
+					Name
+				</dt>
 				<dd>{order.customer.name}</dd>
 			</div>
 
 			<div class="flex items-center justify-between">
-				<dt class="text-muted-foreground">Email</dt>
+				<dt class="flex items-center gap-1.5 text-muted-foreground">
+					<MailIcon aria-hidden class="size-4" />
+					Email
+				</dt>
 				<dd>
-					<a href="mailto:{order.customer.email}">{order.customer.email}</a>
+					<a class="text-accent-foreground underline" href="mailto:{order.customer.email}">
+						{order.customer.email}
+					</a>
 				</dd>
 			</div>
 
 			<div class="flex items-center justify-between">
-				<dt class="text-muted-foreground">Phone</dt>
+				<dt class="flex items-center gap-1.5 text-muted-foreground">
+					<PhoneIcon aria-hidden class="size-4" />
+					Phone
+				</dt>
 				<dd>
-					<a href="tel:{order.customer.phone}">{order.customer.phone}</a>
+					<a class="text-accent-foreground underline" href="tel:{order.customer.phone}">
+						{order.customer.phone}
+					</a>
 				</dd>
 			</div>
 		</dl>
