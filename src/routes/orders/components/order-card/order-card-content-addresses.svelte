59b9c4e681cc3cb65a012c5b17918b98fdd391ed
Ryan Sobol <hello@ryansobol.com>
2024-11-21 11:34:41 -0800

    Fix content spacing of the shipping and billing address

--- src/routes/orders/components/order-card/order-card-content-addresses.svelte
+++ src/routes/orders/components/order-card/order-card-content-addresses.svelte
@@ -9,14 +9,14 @@
 </script>
 
 {#if order}
-	<section aria-label="Order Addresses" class="grid grid-cols-2 gap-4">
+	<section aria-label="Order Addresses" class="flex place-content-between">
 		<section aria-labelledby="shipping-address" class="grid gap-3">
 			<h4 class="font-semibold" id="shipping-address">Shipping Address</h4>
 
-			<address class="grid gap-0.5 not-italic text-muted-foreground">
-				<span>{order.shippingAddress.line1}</span>
-				<span>{order.shippingAddress.line2}</span>
-				<span>{order.shippingAddress.line3}</span>
+			<address class="space-y-0.5 not-italic text-muted-foreground">
+				<div>{order.shippingAddress.line1}</div>
+				<div>{order.shippingAddress.line2}</div>
+				<div>{order.shippingAddress.line3}</div>
 			</address>
 		</section>
 
@@ -24,10 +24,10 @@
 			<h4 class="font-semibold" id="billing-address">Billing Address</h4>
 
 			{#if order.billingAddress}
-				<address class="grid gap-0.5 not-italic text-muted-foreground">
-					<span>{order.billingAddress.line1}</span>
-					<span>{order.billingAddress.line2}</span>
-					<span>{order.billingAddress.line3}</span>
+				<address class="space-y-0.5 not-italic text-muted-foreground">
+					<div>{order.billingAddress.line1}</div>
+					<div>{order.billingAddress.line2}</div>
+					<div>{order.billingAddress.line3}</div>
 				</address>
 			{:else}
 				<div class="text-muted-foreground">Same as shipping address</div>
