3dbe387ca32a4b90b61d4f7bc99c6c300aa340fd
Ryan Sobol <hello@ryansobol.com>
2024-11-23 11:06:53 -0800

    Experiment with max-content width for the order details column

--- src/routes/orders/+page.svelte
+++ src/routes/orders/+page.svelte
@@ -2,6 +2,8 @@
 	import { replaceState } from '$app/navigation';
 	import { page } from '$app/stores';
 
+	import { cn } from '$lib/utils';
+
 	import * as OrderCard from './components/order-card/index.js';
 	import OrdersTabs from './components/orders-tabs.svelte';
 	import ThisMonthCard from './components/this-month-card.svelte';
@@ -23,7 +25,10 @@
 
 <main
 	aria-label="orders"
-	class="grid gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 min-[1358px]:grid-cols-[_minmax(0,_1fr)_minmax(0,max-content)]"
+	class={cn(
+		'grid gap-4 p-4 sm:px-6 sm:py-0 md:gap-8',
+		order ? 'min-[1358px]:grid-cols-[_minmax(0,_1fr)_max-content]' : 'grid-cols-1'
+	)}
 >
 	<div class="grid auto-rows-max items-start gap-4 md:gap-8">
 		<div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
