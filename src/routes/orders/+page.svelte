a8c24463926d87aba3ec4a96991ef6792da8a7c6
Ryan Sobol <hello@ryansobol.com>
2024-11-24 11:53:30 -0800

    Fix exit transtion final jank  when hiding the OrderCard component

--- src/routes/orders/+page.svelte
+++ src/routes/orders/+page.svelte
@@ -14,7 +14,7 @@
 	let order: Order | undefined = $derived($page.data.order);
 </script>
 
-<main aria-label="orders" class="flex flex-col gap-8 p-4 sm:px-6 sm:py-0 xl:flex-row">
+<main aria-label="orders" class="flex flex-col p-4 sm:px-6 sm:py-0 xl:flex-row">
 	<div class="grid flex-1 gap-4 lg:gap-8">
 		<div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
 			<YourOrdersCard />
@@ -30,7 +30,7 @@
 	{#if order}
 		<aside
 			aria-label="order details"
-			class="text-nowrap"
+			class="ml-8 text-nowrap"
 			transition:slide={{ axis: 'x', duration: 150, easing: quadOut }}
 		>
 			<OrderCard.Root>
