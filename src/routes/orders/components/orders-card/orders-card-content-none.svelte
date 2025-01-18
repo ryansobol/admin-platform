64e4e945c5d4443b135aca9864e904b677a7a345
Ryan Sobol <hello@ryansobol.com>
2025-01-18 15:09:20 -0800

    Adjust orders card content none component for new container dimensions

--- src/routes/orders/components/orders-card/orders-card-content-none.svelte
+++ src/routes/orders/components/orders-card/orders-card-content-none.svelte
@@ -4,15 +4,15 @@
 	import * as Card from '$lib/components/ui/card/index.ts';
 </script>
 
-<Card.Content
-	class="flex min-h-fit flex-row content-center items-center justify-center gap-8 py-28"
->
-	<FilterX class="mb-4 size-32 text-muted-foreground" />
+<Card.Content class="flex h-full flex-row content-center items-center justify-center gap-8">
+	<FilterX class="mb-4 hidden text-muted-foreground sm:block sm:size-20 md:size-28" />
 
 	<div class="flex flex-col items-center justify-center gap-2">
-		<p class="text-center text-xl">No orders match your current filter settings.</p>
+		<p class="text-center text-base sm:text-lg md:text-xl">
+			No orders match your current filter settings.
+		</p>
 
-		<p class="text-center text-lg text-muted-foreground">
+		<p class="text-center text-sm text-muted-foreground sm:text-base md:text-lg">
 			Try adjusting your filters to see more orders.
 		</p>
 	</div>
